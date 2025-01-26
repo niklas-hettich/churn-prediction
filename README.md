2214c1cad7c3cdfa714c6d64a421f333643d77f2
ge87fen <niklas.hettich@tum.de>
2025-01-26 17:51:34 +0100

    added Dockerfile

---
+++ README.md
@@ -0,0 +1,11 @@
+run this command if the backend is changed:
+
+gcloud run deploy mindfuel-application-backend \
+  --source . \
+  --platform managed \
+  --region europe-west1 \
+  --allow-unauthenticated \
+  --memory 2Gi \
+  --min-instances=1 \
+  --port 5000
+
--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from joblib import load
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -39,5 +40,13 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
+@app.route('/health', methods=['GET'])
+def health():
+    return "Ok", 200
+
+# if __name__ == '__main__':
+#    app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
